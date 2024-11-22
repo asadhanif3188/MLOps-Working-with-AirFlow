8297ec193de4babdd5b1673601b9314162182ab2
Asad Hanif <asadhanif3188@gmail.com>
2024-11-22 15:33:42 +0500

    Task functions are added in maths_operations.py

--- dags/maths_operations.py
+++ dags/maths_operations.py
@@ -9,3 +9,28 @@ Task 5: Compute the square of the result
 
 """
 
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+# Define the functions 
+
+def start_number(**context):
+    context['ti'].xcom_push(key='current_value', value=10)
+
+def add_five(**context):
+    current_value = context['ti'].xcom_pull(key = 'current_value')
+    context['ti'].xcom_push(key = 'add_five', value = current_value + 5)
+
+def multiply_by_two(**context):
+    current_value = context['ti'].xcom_pull(key = 'add_five')
+    context['ti'].xcom_push(key = 'multiply_by_two', value = current_value * 2)
+
+def subtract_three(**context):
+    current_value = context['ti'].xcom_pull(key = 'multiply_by_two')
+    context['ti'].xcom_push(key = 'subtract_three', value = current_value - 3)
+
+def square(**context):
+    current_value = context['ti'].xcom_pull(key = 'subtract_three')
+    context['ti'].xcom_push(key = 'square', value = current_value ** 2)
+
